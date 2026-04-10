e85891384b3dc8a8733f42f21d036503910e16a8
Baobyun <25099838g@connect.polyu.hk>
2026-04-10 23:46:25 +0800

    add:添加看板

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 一键自动化
 """
-
+import subprocess
 from core.performance import PerformanceTester
 from core.metrics import RagEvaluator
 
@@ -24,5 +24,16 @@ def run_full_pipeline():
     print(f"RAG 检索精度 (nDCG): {ndcg:.4f}")
     print("全链路评测任务完成,报告已生成。")
 
+
+def generate_report():
+    print(" 正在生成 Allure 自动化报表")
+
+    # 1. 运行 pytest 并将结果存入 temp_results 文件夹
+    subprocess.run("pytest tests/ --alluredir=./reports/allure_raw", shell=True)
+
+    # 2. 将原始结果转化为 HTML 报表并自动打开
+    subprocess.run("allure serve ./reports/allure_raw", shell=True)
+
 if __name__ == "__main__":
     run_full_pipeline()
+    generate_report()
